44afe8f6dc68dec8259240695f1d8e3e6691e65d
Bertuğ YILMAZ <yilmazbertug@hotmail.com>
2017-05-01 22:31:17 +0300

    Added CircularArrayTypedQueue Class

---
+++ TurgutluBank/CircularArrayTypedQueue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurgutluBank
+{
+    public class CircularArrayTypedQueue : IQueue
+    {
+        
+        public void Insert(object o)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool isEmpty()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Peek()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Remove()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
