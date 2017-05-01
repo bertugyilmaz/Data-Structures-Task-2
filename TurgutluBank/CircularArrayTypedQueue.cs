07194b49d0521a028c5568713459081c89ef5bb2
Bertuğ YILMAZ <yilmazbertug@hotmail.com>
2017-05-01 22:44:16 +0300

    Added Queue Classes

--- TurgutluBank/CircularArrayTypedQueue.cs
+++ TurgutluBank/CircularArrayTypedQueue.cs
@@ -8,25 +8,62 @@ namespace TurgutluBank
 {
     public class CircularArrayTypedQueue : IQueue
     {
-        
+        private object[] Queue;
+        private int front = -1;
+        private int rear = -1;
+        private int size = 0;
+        private int count = 0;
+
+        public CircularArrayTypedQueue(int size)
+        {
+            this.size = size;
+            Queue = new object[size];
+        }
         public void Insert(object o)
         {
-            throw new NotImplementedException();
+            if (count == size)
+                throw new Exception("Queue dolu.");
+
+            if (front == -1)
+                front = 0;
+
+            if (rear == size - 1)
+            {
+                rear = 0;
+                Queue[rear] = o;
+            }
+            else
+                Queue[++rear] = o;
+
+            count++; 
         }
 
         public bool isEmpty()
         {
-            throw new NotImplementedException();
+            return (count == 0);
         }
 
         public object Peek()
         {
-            throw new NotImplementedException();
+            return Queue[front];
         }
 
         public object Remove()
         {
-            throw new NotImplementedException();
+            if (isEmpty())
+                throw new Exception("Queue boş.");
+
+            object temp = Queue[front];
+            Queue[front] = null;
+
+            if (front == size - 1)
+                front = 0;
+            else
+                front++;
+
+            count--;
+
+            return temp;
         }
     }
 }
---
+++ TurgutluBank/PriorityQueue.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurgutluBank
+{
+    public class PriorityQueue : IQueue
+    {
+        private object[] Queue;
+        private int front = -1;
+        
+        private int size = 0;
+        private int count = 0;
+
+        public PriorityQueue(int size)
+        {
+            this.size = size;
+            Queue = new object[size];
+        }
+        public void Insert(object o)
+        {
+            if (count == size)
+            {
+                throw new Exception("Queue is full");
+            }
+
+            if (isEmpty())
+            {
+                front++;
+                Queue[front] = o;
+                count++;
+            }
+            else
+            {
+                int i;
+                for (i = count - 1; i >= 0; i--)
+                {
+                    if ((int)o > (int)Queue[i])
+                        Queue[i + 1] = Queue[i];
+                    else
+                        break;
+                }
+                Queue[i + 1] = o;
+                front++;
+                count++;
+            }
+        }
+
+        public object Remove()
+        {
+            if (this.isEmpty())
+            {
+                throw new Exception("Queue is empty...");
+            }
+            object temp = Queue[front];
+            Queue[front] = null;
+            front--;
+            count--;
+            return temp;
+        }
+
+        public object Peek()
+        {
+            return Queue[front];
+        }
+
+        public bool isEmpty()
+        {
+            return (count == 0);
+        }
+    }
+}
